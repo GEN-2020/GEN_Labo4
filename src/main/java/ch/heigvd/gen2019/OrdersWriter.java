59f3614dc87805e93fd2e16b724a827961bd40e0
Kleeer <delhomme.claire.ts7@gmail.com>
2020-05-26 15:36:08 +0200

    bad smell: temp variable, refactor: replace temp w query

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -36,9 +36,7 @@ public class OrdersWriter {
         sb.append(", ");
         sb.append("\"products\": [");
         for (int j = 0; j < order.getProductsCount(); j++) {
-            Product product = order.getProduct(j);
-
-            getProductContents(sb, product);
+            getProductContents(sb, order.getProduct(j));
         }
     }
 
