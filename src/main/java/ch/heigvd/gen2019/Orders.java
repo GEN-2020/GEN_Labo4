3f0d82983ee2122bd58dca6e8941ff77db029690
Kleeer <delhomme.claire.ts7@gmail.com>
2020-05-26 17:03:48 +0200

    refactor : Orders provides display w method toString, previously delegated to OrdersWriter

--- src/main/java/ch/heigvd/gen2019/Orders.java
+++ src/main/java/ch/heigvd/gen2019/Orders.java
@@ -17,4 +17,27 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < this.getOrdersCount(); i++) {
+            Order order = this.getOrder(i);
+            sb.append(order.toString());
+
+            if (order.getProductsCount() > 0) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+
+            sb.append("]");
+            sb.append("}, ");
+        }
+
+        if (this.getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
+    }
 }
--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -8,25 +8,7 @@ public class OrdersWriter {
     }
 
     public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append(order.toString());
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
+        return orders.toString();
     }
 
     private String getSizeFor(Product product) {
