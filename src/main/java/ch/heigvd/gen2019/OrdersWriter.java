0fa63330b6691bddaf3678ffd740ab62a99ec601
Kleeer <delhomme.claire.ts7@gmail.com>
2020-05-26 16:49:34 +0200

    refactor : Product provides display w method toString, previously delegated to OrdersWriter

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -41,26 +41,7 @@ public class OrdersWriter {
     }
 
     private void getProductContents(StringBuffer sb, Product product) {
-        sb.append("{");
-        sb.append("\"code\": \"");
-        sb.append(product.getCode());
-        sb.append("\", ");
-        sb.append("\"color\": \"");
-        sb.append(getColorFor(product));
-        sb.append("\", ");
-
-        if (product.getSize() != Size.NO_SIZE) {
-            sb.append("\"size\": \"");
-            sb.append(getSizeFor(product));
-            sb.append("\", ");
-        }
-
-        sb.append("\"price\": ");
-        sb.append(product.getPrice());
-        sb.append(", ");
-        sb.append("\"currency\": \"");
-        sb.append(product.getCurrency());
-        sb.append("\"}, ");
+        sb.append(product.toString());
     }
 
     private String getSizeFor(Product product) {
